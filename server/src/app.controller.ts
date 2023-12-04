b9e217300942955fda8d93e7df9866c277db2d65
pullso <pullso.code@gmail.com>
2023-12-04 22:34:57 +0300

    added prisma

--- server/src/app.controller.ts
+++ server/src/app.controller.ts
@@ -1,7 +1,7 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 import { ApiOkResponse, ApiProperty } from '@nestjs/swagger';
-
+import { DbService } from './db/db.service';
 class HelloMessageDto {
   @ApiProperty()
   message: string;
@@ -9,11 +9,17 @@ class HelloMessageDto {
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private dbService: DbService,
+  ) {}
 
   @Get()
   @ApiOkResponse({ type: HelloMessageDto })
-  getHello(): HelloMessageDto {
+  async getHello(): Promise<HelloMessageDto> {
+    const users = await this.dbService.user.findMany({});
+    console.log(users);
+
     return { message: this.appService.getHello() };
   }
 }
