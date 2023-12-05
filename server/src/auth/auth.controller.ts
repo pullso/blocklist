63df04d14478faeaae0faa6eec7998affe33e0ab
pullso <pullso.code@gmail.com>
2023-12-05 20:15:00 +0300

    update controller

--- server/src/auth/auth.controller.ts
+++ server/src/auth/auth.controller.ts
@@ -1,20 +1,48 @@
-import { Controller, Get, HttpCode, HttpStatus, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Post,
+} from '@nestjs/common';
+import { SignInBodyDto, SignUpBodyDto, getSessionInfoDto } from './dto';
+import { ApiCreatedResponse, ApiOkResponse } from '@nestjs/swagger';
 
 @Controller('auth')
 export class AuthController {
   constructor() {}
 
   @Post('sign-up')
-  signUp() {}
+  @ApiCreatedResponse()
+  signUp(@Body() body: SignUpBodyDto) {
+    return null;
+  }
 
   @Post('sign-in')
+  @ApiOkResponse()
   @HttpCode(HttpStatus.OK)
-  signIn() {}
+  signIn(@Body() body: SignInBodyDto) {
+    return null;
+  }
 
   @Post('sign-out')
+  @ApiOkResponse()
   @HttpCode(HttpStatus.OK)
   singOut() {}
 
   @Get('session')
-  getSessionInfo() {}
+  @ApiOkResponse({
+    type: getSessionInfoDto,
+  })
+  getSessionInfo() {
+    return null;
+  }
+}
+function ApiCreateResponse(): (
+  target: AuthController,
+  propertyKey: 'signUp',
+  descriptor: TypedPropertyDescriptor<(body: SignUpBodyDto) => void>,
+) => void | TypedPropertyDescriptor<(body: SignUpBodyDto) => void> {
+  throw new Error('Function not implemented.');
 }
