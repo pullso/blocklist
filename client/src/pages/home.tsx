eacd95c698382b8e0ddbd2cc880c503adc6b7e0c
pullso <pullso.code@gmail.com>
2023-12-15 21:25:56 +0300

    text field has been added

--- client/src/pages/home.tsx
+++ client/src/pages/home.tsx
@@ -1,15 +1,16 @@
-import {Inter} from 'next/font/google'
+import { Inter } from "next/font/google";
 import { authControllerGetSessionInfo } from "@/shared/api/generated";
-import {useQuery} from "@tanstack/react-query";
-import {UiButton} from "@/shared/ui/ui-button";
+import { useQuery } from "@tanstack/react-query";
+import { UiButton } from "@/shared/ui/ui-button";
+import { UiTextField } from "@/shared/ui/ui-text-field";
 
-const inter = Inter({subsets: ['latin']})
+const inter = Inter({ subsets: ["latin"] });
 
 export default function HomePage() {
-  const {data} = useQuery({
-    queryKey: ['session'],
-    queryFn: () => authControllerGetSessionInfo()
-  })
+  const { data } = useQuery({
+    queryKey: ["session"],
+    queryFn: () => authControllerGetSessionInfo(),
+  });
 
   return (
     <main
@@ -19,7 +20,18 @@ export default function HomePage() {
       <UiButton variant="primary">test</UiButton>
       <UiButton variant="secondary">test</UiButton>
       <UiButton variant="outlined">sign out</UiButton>
-      <UiButton disabled variant="outlined">sign out</UiButton>
+      <UiButton disabled variant="outlined">
+        sign out
+      </UiButton>
+      <UiTextField
+        label="label text"
+        inputProps={{ placeholder: "placeholder text" }}
+      />
+      <UiTextField
+        error="label text"
+        inputProps={{ placeholder: "placeholder text" }}
+      />
+      <UiTextField inputProps={{ placeholder: "placeholder text" }} />
     </main>
-  )
+  );
 }
